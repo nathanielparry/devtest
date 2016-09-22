7ee569bc414efb46a61815197458a0c7df335531
nathanielparry <nateparry@hotmail.com>
2016-09-22 03:25:30 -0600

    5.5 hrs
    Created Tile page
    cleaned up files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
 $(document).ready(function () {
-    // Problem: mainDisplay needs to show images clicked
 
-
-
+    // Display clicked image in #mainDisplay
     $('#thumbnails img').click(function (event) {
-        var $photobomb = $(this).attr('src');
+        var $photobomb = $(this).prop('src');
         var $title = $(this).prop('title');
         var $caption = $(this).attr('caption');
 
-        $('#mainDisplay img').attr('src', $photobomb);
-
+        $('#mainDisplay img').prop('src', $photobomb);
         $('.imgTitle').text($title);
         $('.imgCaption').text($caption);
     });
 
+    // Populate background image for tiles to avoid having to manually resize images for each tile
     $('.tileImg').each(function () {
         var $background = $(this).attr('image');
-        console.log($background);
         $(this).css({
             'background': 'url(' + $background + ')'
             , 'background-size': 'cover'
@@ -24,4 +21,13 @@ $(document).ready(function () {
         });
     });
 
+    // Move Tile video thumb to main display area
+
+    var $mainWidth = $('main').width();
+
+    $('#vidTeaser img').click(function (event) {
+        $(this).hide();
+        $('#vidTeaser iframe').show().css('height', $mainWidth * 0.562);
+        $('.vidInfo').addClass('full');
+    });
 });
